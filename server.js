b218da3cd6d8797aeeb8e0fdce7be87ceeb06739
Fernando Martínez Ortiz <fermtz020700@hotmail.com>
2021-10-20 22:12:55 -0500

    finished clear functionality

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -58,6 +58,12 @@ app.post("/api/tables", function(req, res) {
     }
     
 });
+
+app.post("/api/erase", function(req, res){
+    tables = []
+    waitlist = []
+    res.json(true)
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
